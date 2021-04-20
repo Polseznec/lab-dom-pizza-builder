b507189a1b9b7e1a51e8de21630fa84e9271d669
Pol Seznec <johndoe@example.com>
2021-04-21 00:24:16 +0200

    Index.js V1 No last Iteration 4

--- js/index.js
+++ js/index.js
@@ -44,23 +44,70 @@ function renderPepperoni() {
 
 function renderMushrooms() {
   // Iteration 1: set the visibility of `<section class="mushroom">`
+  document.querySelectorAll('.mushroom').forEach(oneMush => {
+    if (state.mushrooms) {
+      oneMush.style.visibility = 'visible';
+    } else {
+      oneMush.style.visibility = 'hidden';
+    }
+  });
 }
 
 function renderGreenPeppers() {
   // Iteration 1: set the visibility of `<section class="green-pepper">`
+  document.querySelectorAll('.green-pepper').forEach(oneGreenPepper => {
+    if (state.greenPeppers) {
+      oneGreenPepper.style.visibility = 'visible';
+    } else {
+      oneGreenPepper.style.visibility = 'hidden';
+    }
+  });
 }
 
 function renderWhiteSauce() {
   // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
+const whiteSauce = document.querySelector(".sauce")
+console.log(whiteSauce);
+whiteSauce.classList.toggle("sauce-white");
 }
+//   document.querySelector('.sauce.sauce-white').forEach(oneSauce =>{
+//     if (state.sauce.sauceWhite){
+//       oneSauce.style.visibility = 'visible';
+//     }else{
+//       oneSauce.style.visibility = 'hidden';
+//     }
+//   })
+// }
+//state.sauce.sauceWhite != state.sauce.sauceWhite;
+
 
 function renderGlutenFreeCrust() {
   // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
+  const crust1 = document.querySelector(".crust")
+  console.log(crust1);
+  crust1.classList.toggle("crust-gluten-free");
 }
 
 function renderButtons() {
   // Iteration 3: add/remove the class "active" of each `<button class="btn">`
-}
+  const active1 = document.querySelectorAll(".btn");
+  console.log(active1);
+  if (state.pepperoni === true) {
+    document.querySelector(".btn-pepperoni").classList.add("active");
+  } else {
+    document.querySelector(".btn-pepperoni").classList.remove("active");
+  };
+  if (state.mushrooms === true) {
+    document.querySelector(".btn-mushrooms").classList.add("active");
+  } else {
+    document.querySelector(".btn-mushrooms").classList.remove("active");
+  };
+  if (state.greenPeppers === true) {
+    document.querySelector(".btn-green-peppers").classList.add("active");
+  } else {
+    document.querySelector(".btn-green-peppers").classList.remove("active");
+  };
+  }
 
 function renderPrice() {
   // Iteration 4: change the HTML of `<aside class="panel price">`
@@ -75,9 +122,51 @@ document.querySelector('.btn.btn-pepperoni').addEventListener('click', () => {
 });
 
 // Iteration 1: Add click event listener on `<button class="btn btn-mushrooms">`
-
+document.querySelector('.btn.btn-mushrooms').addEventListener('click', () => {
+  state.mushrooms = !state.mushrooms;
+  renderEverything();
+});
 // Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
-
+document.querySelector('.btn.btn-green-peppers').addEventListener('click', () => {
+  state.greenPeppers = !state.greenPeppers;
+  renderEverything();
+});
 // Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
+document.querySelector('.btn.btn-sauce').addEventListener('click', () => {
+  state.whiteSauce = !state.whiteSauce;
+  renderEverything();
+});
+// document.querySelector('.btn.btn-sauce active').addEventListener('click', () => {
+//   state.sauce.sauceWhite != state.sauce.sauceWhite;
+//   renderEverything();
+// })
 
 // Iteration 2: Add click event listener on `<button class="btn btn-crust">`
+document.querySelector('.btn.btn-crust').addEventListener('click', () => {
+  state.crust1 = !state.crust1;
+  renderEverything();
+});
+
+
+// Iteration 4 : Ingredients and prices menu
+// ."panel price" = Class of menu
+
+// function renderPrice()
+// document.{forEach((menuElement)=>{ 
+
+// });
+// }
+
+/////////////////////////////////////////
+// If (state.pepperoni){
+
+// }
+
+
+
+// select price from a string = .subString (a, b)
+
+// document.querySelector(".panel price .ul .li").addEventListener("click", () => {
+//   state.mushroom = !state.mushroom;
+//   renderEverything();
+// });
